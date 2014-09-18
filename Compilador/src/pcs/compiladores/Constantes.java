3541ddad2a166c7cba0f492ba5e6666215ea90c7
Erico <augusto.ericosilva@gmail.com>
2014-09-17 21:00:41 -0300

    Almost done

---
+++ Compilador/src/pcs/compiladores/Constantes.java
@@ -0,0 +1,12 @@
+package pcs.compiladores;
+
+public class Constantes {
+	
+	public static String COMENTARIO = "#";
+	public static String BRANCO = "branco";
+	public static String NUMERO = "number";
+	public static String DECIMAL = "dec";
+	public static String HEXADECIMAL = "hex";
+	public static String ID = "id";
+
+}
